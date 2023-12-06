6b5b26a9faaa51b591baba85915eae201ae420ef
deepankarranjandey <137293937+deepankarranjandey@users.noreply.github.com>
2023-12-06 16:15:29 +0530

    added post method

--- tests/apitestone.spec.ts
+++ tests/apitestone.spec.ts
@@ -18,4 +18,23 @@ test('Check if the end point is working', async ({ request }) => {
 
 );
 
+test('create a new user in the database', async ({ request }) => {  
+
+    const response = await request.post('https://reqres.in/api/users', {
+
+    data:{
+        name: "morpheus",
+        job: "leader"
+    }
+    });
+
+    expect(response.status()).toBe(201);
+    const responseBody = await response.json();
+
+    expect(responseBody).toHaveProperty('name', 'morpheus');
+    expect(responseBody).toHaveProperty('job', 'leader');
+    expect(responseBody).toHaveProperty('id');
+    expect(responseBody).toHaveProperty('createdAt');
+}
+);
 
