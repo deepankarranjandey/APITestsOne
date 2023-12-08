6383f7ef7a2fa1463400b07cc84428be584aac02
deepankarranjandey <137293937+deepankarranjandey@users.noreply.github.com>
2023-12-08 18:21:43 +0530

    added delete method

--- tests/apitestone.spec.ts
+++ tests/apitestone.spec.ts
@@ -57,3 +57,11 @@ test('update a user in the database', async ({ request }) => {
     }
 );
 
+test('delete a user in the database', async ({ request }) => {
+    
+        const response = await request.delete('https://reqres.in/api/users/2');
+    
+        expect(response.status()).toBe(204);
+    }   
+);
+
